b474be59e065c1ed0492b430587f19f594079dbc
KuroKaizer <manueludouni@gmail.com>
2023-03-26 19:52:00 -0400

    Configuración de las vistas

--- MartFarming/autenticacion/urls.py
+++ MartFarming/autenticacion/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
-#from .views import Resgistro,login
 
 urlpatterns = [
-    #path('', views.Registro, name='Autenticar'),
-    path('', views.login, name='Autenticar'),
+    path('registrar/', views.registro),
+    path('login/', views.login)
 ]
--- MartFarming/autenticacion/views.py
+++ MartFarming/autenticacion/views.py
@@ -3,17 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def Registro(request):
-    return render(request, 'Usuarios/registro.html')
+def registro(request):
+    return render(request, "Usuarios/registro.html")
 
 
 def login(request):
-    return render(request, 'Usuarios/login.html')
+    return render(request, "Usuarios/login.html")
 
-"""class Resgistro(View):
-    
-    def get(self, request):
-        return render(request, 'Usuarios/registro.html')
-
-    def post(self, request):
-        pass"""
