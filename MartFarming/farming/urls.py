9a43a82b4bd39de1fc78fb66e0941c39f99548a5
Manuel <manueludouni@gmail.com>
2023-04-04 14:07:44 -0400

    Reajuste de las urls

--- MartFarming/farming/urls.py
+++ MartFarming/farming/urls.py
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('ubicacion/', views.ubicacion, name='ubicacion')
-=======
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('ubicacion/', views.ubicacion, name='ubicacion')
->>>>>>> 0f31af80fd51b49155b3cbb4f9f4ad95329b6ab7
 ]
--- MartFarming/farming/views.py
+++ MartFarming/farming/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -8,14 +7,3 @@ def index(request):
 
 def ubicacion(request):
     return render(request, "farming/ubicacion.html")
-=======
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    return render(request, "farming/index.html")
-
-def ubicacion(request):
-    return render(request, "farming/ubicacion.html")
->>>>>>> 0f31af80fd51b49155b3cbb4f9f4ad95329b6ab7
