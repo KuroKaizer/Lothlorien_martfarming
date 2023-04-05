f757cf2be7e5b8a8b4b4b9dc40ccf66489a4bfb6
Manuel <manueludouni@gmail.com>
2023-04-05 11:19:06 -0400

    Cambio en la configuración de la base de datos

--- MartFarming/MartFarming/settings.py
+++ MartFarming/MartFarming/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "MartFarming.wsgi.application"
 
 DATABASES = DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'farming_db',
+        'USER': 'madraton',
+        'PASSWORD': 'kakaroto',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
